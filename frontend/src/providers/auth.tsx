f718cd4b9369a92b5b218bd3b89cca1088c17d3c
rtokkie <natto.and.eggs@gmail.com>
2022-09-25 13:40:45 +0900

    u

--- frontend/src/providers/auth.tsx
+++ frontend/src/providers/auth.tsx
@@ -1,16 +1,33 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
+import { axios } from '@/lib/axios';
 import { WithChildren } from '@/types';
 import { assertDefined } from '@/utils/assert-defined';
 
+type User = { name: string };
+
 type State = {
   initialized: boolean;
+  user: User | undefined;
+  setUser: (v: User | undefined) => void;
 };
 
 const useAuthProvider = () => {
   const [initialized, setInitialized] = useState(false);
+  const [user, setUser] = useState<User>();
 
-  return { initialized };
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await axios.get<User>('user');
+        setUser(res.data);
+      } finally {
+        setInitialized(true);
+      }
+    })();
+  }, []);
+
+  return { initialized, user, setUser };
 };
 
 const AuthContext = createContext<State | undefined>(undefined);
@@ -18,6 +35,7 @@ const AuthContext = createContext<State | undefined>(undefined);
 export const AuthProvider = ({ children }: WithChildren) => {
   const state = useAuthProvider();
   if (!state) return null;
+  if (!state.initialized) return null;
   return <AuthContext.Provider value={state}>{children}</AuthContext.Provider>;
 };
 
