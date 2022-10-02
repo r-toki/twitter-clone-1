e484c5c5f8d9be077104d7e061cb49556dcaf74a
rtokkie <natto.and.eggs@gmail.com>
2022-10-02 13:02:14 +0900

    a

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,50 +1,65 @@
-mod application;
-mod domain;
-mod infrastructure;
-mod modules;
-mod presentation;
+// mod application;
+// mod domain;
+// mod infrastructure;
+// mod modules;
+// mod presentation;
 
-use crate::infrastructure::config::CONFIG;
-use crate::modules::Modules;
+// use crate::infrastructure::config::CONFIG;
+// use crate::modules::Modules;
 
-use actix_cors::Cors;
-use actix_web::{http, middleware::Logger, web::Data, App, HttpServer};
-use dotenv::dotenv;
-use sqlx::PgPool;
-use std::sync::Arc;
+// use actix_cors::Cors;
+// use actix_web::{http, middleware::Logger, web::Data, App, HttpServer};
+// use dotenv::dotenv;
+// use sqlx::PgPool;
+// use std::sync::Arc;
+
+// #[actix_web::main]
+// async fn main() -> Result<(), std::io::Error> {
+//     dotenv().ok();
+//     env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
+
+//     let host = &CONFIG.host;
+//     let port = &CONFIG.port;
+//     let database_url = &CONFIG.database_url;
+//     let frontend_origin = &CONFIG.frontend_origin;
+
+//     let pool = PgPool::connect(database_url).await.unwrap();
+
+//     let modules = Arc::new(Modules::new(pool));
+
+//     HttpServer::new(move || {
+//         let cors = Cors::default()
+//             .allowed_origin(&frontend_origin)
+//             .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
+//             .allowed_headers(vec![
+//                 http::header::AUTHORIZATION,
+//                 http::header::ACCEPT,
+//                 http::header::CONTENT_TYPE,
+//             ])
+//             .max_age(3_600);
+
+//         App::new()
+//             .app_data(Data::new(modules.clone()))
+//             .wrap(Logger::default())
+//             .wrap(cors)
+//             .configure(presentation::init)
+//     })
+//     .bind(format!("{}:{}", host, port))?
+//     .run()
+//     .await
+// }
+
+use actix_web::{get, App, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    format!("HELLO WORLD!")
+}
 
 #[actix_web::main]
-async fn main() -> Result<(), std::io::Error> {
-    dotenv().ok();
-    env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
-
-    let host = &CONFIG.host;
-    let port = &CONFIG.port;
-    let database_url = &CONFIG.database_url;
-    let frontend_origin = &CONFIG.frontend_origin;
-
-    let pool = PgPool::connect(database_url).await.unwrap();
-
-    let modules = Arc::new(Modules::new(pool));
-
-    HttpServer::new(move || {
-        let cors = Cors::default()
-            .allowed_origin(&frontend_origin)
-            .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
-            .allowed_headers(vec![
-                http::header::AUTHORIZATION,
-                http::header::ACCEPT,
-                http::header::CONTENT_TYPE,
-            ])
-            .max_age(3_600);
-
-        App::new()
-            .app_data(Data::new(modules.clone()))
-            .wrap(Logger::default())
-            .wrap(cors)
-            .configure(presentation::init)
-    })
-    .bind(format!("{}:{}", host, port))?
-    .run()
-    .await
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
